a2ca75a96d1e2f6ee4d95d4e7af24df4bfd7763f
Pedro Bruel <pedro.bruel@gmail.com>
2015-05-19 13:51:02 -0300

    Added gitignore, started implementing run.

--- src/tuner/nvcc_flags_tuner.py
+++ src/tuner/nvcc_flags_tuner.py
@@ -5,45 +5,56 @@ from opentuner import IntegerParameter
 from opentuner import MeasurementInterface
 from opentuner import Result
 
+import argparse
+
+argparser = argparse.ArgumentParser(parents=opentuner.argparsers())
+argparser.add_argument( "-f", "--file",
+                        dest = "filename",
+                        required = True,
+                        help = "A file to tune.")
+
 # Specify compiler options:
 NVCC_NAME = "-Xcompiler"
 # name
-NVCC_FLAGS = [ "--no-align-double=",
-               "--relocatable-device-code=",
-               "--use_fast_math=",
-               "--ftz=",
-               "--prec-div=",
-               "--prec-sqrt=",
-               "--fmad " ]
+NVCC_FLAGS = [ "nvcc:--no-align-double=",
+               "nvcc:--relocatable-device-code=",
+               "nvcc:--use_fast_math=",
+               "nvcc:--ftz=",
+               "nvcc:--prec-div=",
+               "nvcc:--prec-sqrt=",
+               "nvcc:--fmad " ]
 # { name : [ args ] }
-NVCC_PARAMS = { "--default-stream="   : [ "legacy", "null", "per-thread" ],
-                "--gpu-architecture=" : [ "sm_20", "sm_21", "sm_30", "sm_32",
+NVCC_PARAMS = { "nvcc:--default-stream="   : [ "legacy", "null", "per-thread" ],
+                "nvcc:--gpu-architecture=" : [ "sm_20", "sm_21", "sm_30", "sm_32",
                                           "sm_35", "sm_50", "sm_52" ] }
 # (name, min, max)
-NVCC_NUM_PARAMS = [ ( "--maxrregcount=", 16, 63 ) ]
+NVCC_NUM_PARAMS = [ ( "nvcc:--maxrregcount=", 16, 63 ) ]
 # Specify ptxas options:
 PTXAS_NAME = "-Xptxas "
 # name
-PTXAS_FLAGS  = [ "--allow-expensive-optimizations=",
-                 "--def-store-cache=",
-                 "--disable-optimizer-consts=",
-                 "--force-load-cache=",
-                 "--force-store-cache=",
-                 "--fmad=" ]
+PTXAS_FLAGS  = [ "ptxas:--allow-expensive-optimizations=",
+                 "ptxas:--def-store-cache=",
+                 "ptxas:--disable-optimizer-consts=",
+                 "ptxas:--force-load-cache=",
+                 "ptxas:--force-store-cache=",
+                 "ptxas--fmad=" ]
 # { name : [ args ] }
-PTXAS_PARAMS = { "--def-load-cache=" : [ "ca", "cg", "cv", "cs" ],
-                 "--gpu-name="       : [ "compute_20", "compute_30",
+PTXAS_PARAMS = { "ptxas:--def-load-cache=" : [ "ca", "cg", "cv", "cs" ],
+                 "ptxas:--gpu-name="       : [ "compute_20", "compute_30",
                                          "compute_35", "compute_50",
                                          "compute_52", "sm_20",
                                          "sm_21", "sm_30", "sm_32",
                                          "sm_35", "sm_50", "sm_52" ],
-                 "--opt-level="      : [ "0", "1", "2", "3" ] }
+                 "ptxas:--opt-level="      : [ "0", "1", "2", "3" ] }
 # ( name, min, max )
-PTXAS_NUM_PARAMS = [ ( "_ptxas_--maxrregcount=", 16, 63 ) ]
+PTXAS_NUM_PARAMS = [ ( "ptxas:--maxrregcount=", 16, 63 ) ]
 # Specify NVLINK options:
 NVLINK_NAME = "-Xnvlink "
 # name
-NVLINK_FLAGS = [ "--preserve-relocs=" ]
+NVLINK_FLAGS = [ "nvlink:--preserve-relocs=" ]
+
+NVCC_CMD = "nvcc "
+NVCC_END = "-o "
 
 class NvccFlagsTuner(MeasurementInterface):
     def manipulator(self):
@@ -62,9 +73,16 @@ class NvccFlagsTuner(MeasurementInterface):
         return manipulator
 
     def run(self, desired_result, input, limit):
-        print desired_result.configuration.data
-        return Result(time=0)
+        cfg = desired_result.configuration.data
+
+        cmd = NVCC_CMD + NVCC_NAME
+        nvcc_flags = [ (key, value) for key,value in cfg.iteritems() if key.startswith("nvcc") ]
+
+#        return Result(time=0)
+        return None
 
 if __name__ == '__main__':
-    argparser = opentuner.default_argparser()
+    args = argparser.parse_args()
+
+    NVCC_END += args.filename
     NvccFlagsTuner.main(argparser.parse_args())
