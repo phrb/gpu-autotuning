8562cd4f40b8b420bd05830265b5d553b9e3e8bf
phrb <pedro.bruel@gmail.com>
2016-01-11 21:20:05 -0200

    Script stub builds a flag histogram.

--- plot_flags.py
+++ plot_flags.py
@@ -16,11 +16,43 @@ font = {'family' : 'serif',
 
 mpl.rc('font', **font)
 
+
+NVCC_FLAGS       = { "nvcc:--no-align-double"                 : ["on", "off"],
+                     "nvcc:--use_fast_math"                   : ["on", "off"],
+                     "nvlink:--preserve-relocs"               : ["on", "off"],
+                     "nvcc:--relocatable-device-code="        : [ "true", "false" ],
+                     "nvcc:--ftz="                            : [ "true", "false" ],
+                     "nvcc:--prec-div="                       : [ "true", "false" ],
+                     "nvcc:--prec-sqrt="                      : [ "true", "false" ],
+                     "ptxas:--fmad="                          : [ "true", "false" ],
+                     "ptxas:--allow-expensive-optimizations=" : [ "true", "false" ],
+                     "nvcc:--gpu-architecture="               : [ "sm_20", "sm_21",
+                                                                  "sm_30", "sm_32", "sm_35" ],
+                     "ptxas:--def-load-cache="                : [ "ca", "cg", "cv", "cs" ],
+                     "ptxas:--opt-level="                     : [ "0", "1", "2", "3" ] }
+PTXAS_NUM_PARAMS = [ ( "ptxas:--maxrregcount=", 16, 63 ) ]
+
+FLAGS = {}
+
+for flag in NVCC_FLAGS:
+    for modifier in NVCC_FLAGS[flag]:
+        FLAGS[flag + modifier] = 0
+
+print FLAGS
+
 file = open("experiments/GTX-680/MatMulShared/size_1024_time_3600/run_0/final_config.json")
 json_file = json.loads(file.read())
 
 print json_file, type(json_file)
 
+for flag in json_file:
+    if flag != "ptxas:--maxrregcount=":
+        print flag
+        parameter = flag + json_file[flag]
+        if parameter in FLAGS:
+            FLAGS[parameter] += 1
+
+print FLAGS
 
 #
 # Navigate directories:
